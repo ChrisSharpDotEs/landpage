8cec54a72ca12151c725dbc7fe0499bf8dc91968
Cristian <82599146+cleverChrisTest@users.noreply.github.com>
2024-05-29 20:02:35 +0100

    Se crea un script para instalar nodejs a su versión más reciente

--- NodeTests/index.js
+++ NodeTests/index.js
@@ -1,5 +1,20 @@
-(function(){
-    console.log("Hola" instanceof Object)
-})()
+class Extractor {
+    #value;
+    constructor(html, pattern){
+        this.#value = this.#extract(html, pattern)
+    }
 
+    getValue(){
+        return this.#value;
+    }
+    
+    #extract(html, pattern){
+        let capture = pattern.exec(html);
+        return capture[1];
+    }
+}
+let html = 'https://www.google.es/?gws_rd=ssl';
 
+let image = new Extractor(html, new RegExp(/.png/));
+
+console.log(image);
