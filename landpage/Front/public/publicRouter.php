b010727f57e00eb25d147e2acc985b468d5088ca
Cristian <82599146+ChrisSharpDotEs@users.noreply.github.com>
2024-08-25 18:14:55 +0100

    Update

--- landpage/Front/public/publicRouter.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$url = $_SERVER['REQUEST_URI'];
-$dir =  str_replace("/var/www/html", "", __DIR__);
-$url = str_replace($dir . "/publicRouter.php?", "", $url);
-
-chdir("../../Back/App/");
-require '../../Back/App/router.php';
--- landpage/PHPConfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-php_ini_file=$(php -i | grep "Loaded Configuration File" | awk '{print $NF}')
-if[ -n "$php_ini_file" ]; then
-	extension_line="extension=mail"
-
-extension_line_commented=";extension=mail"
-
-if ! grep -q "$extension_line" "$php_ini_file"; then
-	echo "$extension_line" | sudo tee -a "$php_ini_file" > 
